ed5e36049871ce8521f87c899843a395b09fe489
Mackenzie Hodgson <dev.mcknz@gmail.com>
2025-04-07 21:56:58 -0400

    Update task1.sh

--- task1.sh
+++ task1.sh
@@ -1,20 +1,20 @@
 #!/bin/bash
 
+#user input 
+echo "Please think of 2 numbers..."
+read -p "Enter the lower number: " a
+read -p "Enter the higher number: " b
+
+#variables
+oddCount=0
+evenCount=0
+oddSum=0
+evenSum=0
+
 #Hexigonal Function Calculaton
 hexagonal_number() {
     echo $(( n * (2 * n - 1) ))
 }
-
-#user input 
-echo "Please think of 2 numbers..."
-read -p "Enter the lower number (a): " a
-read -p "Enter the higher number (b): " b
-
-#variables
-odd_count=0
-even_count=0
-odd_sum=0
-even_sum=0
 
 #finding the hexigonal numbers 
 echo "Hexagonal numbers within the range [$a, $b]:"
@@ -29,12 +29,12 @@ while true; do
     if [ $hex -ge $a ] && [ $hex -le $b ]; then
         if (( hex % 2 == 0 )); then
             echo "$hex (even)"
-            ((even_count++))
-            ((even_sum+=hex))
+            ((evenCount++))
+            ((evenSum+=hex))
         else
             echo "$hex (odd)"
-            ((odd_count++))
-            ((odd_sum+=hex))
+            ((oddCount++))
+            ((oddSum+=hex))
         fi
     fi
     ((n++))
@@ -43,8 +43,8 @@ done
 
 # Print results at bottom 
 echo "\nResults:"
-echo "Total odd hexagonal numbers: $odd_count"
-echo "Total even hexagonal numbers: $even_count"
-echo "Sum of odd hexagonal numbers: $odd_sum"
-echo "Sum of even hexagonal numbers: $even_sum"
+echo "Total odd hexagonal numbers: $oddCount"
+echo "Total even hexagonal numbers: $evenCount"
+echo "Sum of odd hexagonal numbers: $oddSum"
+echo "Sum of even hexagonal numbers: $evenSum"
 
